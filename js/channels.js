11f438979ee7a4da82826c5d3994c517846af886
googelino <43038850+googelino@users.noreply.github.com>
2018-09-28 18:39:51 +0200

    Add files via upload

---
+++ js/channels.js
@@ -0,0 +1,48 @@
+var yummy = {
+  name:"#Yummy",
+  createdOn: new Date(2016, 03, 01),
+  createdBy: "minus.plus.yummy",
+  starred: false,
+  expiresIn: 100*60*1000,
+  messageCount: 999
+};
+
+
+var sevenContinents = {
+  name:"#SevenContinents",
+  createdOn: new Date(2016, 03, 01),
+  createdBy: "minus.plus.yummy",
+  starred: true,
+  expiresIn: 100*60*1000,
+  messageCount: 999
+};
+
+
+var killerApp = {
+  name:"#KillerApp",
+  createdOn: new Date(2016, 03, 01),
+  createdBy: "minus.plus.yummy",
+  starred: false,
+  expiresIn: 100*60*1000,
+  messageCount: 999
+};
+
+
+var firstPersonOnMars = {
+  name:"#FirstPersonOnMars",
+  createdOn: new Date(2016, 03, 01),
+  createdBy: "minus.plus.yummy",
+  starred: true,
+  expiresIn: 100*60*1000,
+  messageCount: 999
+};
+
+
+var octoberfest = {
+  name:"#Octoberfest",
+  createdOn: new Date(2016, 03, 01),
+  createdBy: "minus.plus.yummy",
+  starred: false,
+  expiresIn: 100*60*1000,
+  messageCount: 999
+};
